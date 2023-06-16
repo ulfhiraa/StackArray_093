6e49d3f03f216cd30638c8fc1d76b769605519a9
ulfhiraa <studyiraa@gmail.com>
2023-06-16 15:00:47 +0700

    method pop (menghapus atau mengambil data)

--- StackUsingArray/StackUsingArray.cpp
+++ StackUsingArray/StackUsingArray.cpp
@@ -26,4 +26,14 @@ public:
 
 		return element;
 	} 
+
+	void pop() {
+		if (empty()) { // step 1
+			cout << "\nStack is empty. Cannot Pop." << endl;  // 1.a
+			return; // 1.b
+		}
+		
+		cout << "\nThe popped element is : " << stack_array[top] << endl; // step 2
+		top--; // step 3 decrement
+	}
 };
